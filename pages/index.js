db577fd5625fe7b9e0ce2abade35011a5564c434
Leonardo Honda <leozinhohondams@gmail.com>
2024-11-25 12:05:15 -0400

    Adiciona imagem na pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+const foto_vaquinha = require("../images/vaquinha.png");
+
 function Home() {
-    return <h1>Compra um petisco para vaquinha</h1>;
+    return (
+        <div>
+            <img src={foto_vaquinha.default.src} alt="Vaquinha"/>
+            <h1>Compra um petisco para vaquinha</h1>
+        </div>
+    );
 }
 
 export default Home;
