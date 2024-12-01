8a44362d81d7a9e43bc03b3de8d7d83e847bb1c4
Leonardo Honda <83879739+LeonardoLopesHonda@users.noreply.github.com>
2024-12-01 02:28:30 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do Tabnews para aprimorar habilidades de programação
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ const foto_vaquinha_2 = require("../assets/images/vaquinha_2.jpg");
 const foto_vaquinha_3 = require("../assets/images/vaquinha_3.jpg");
 
 function Home() {
-    return (
-        <main>
-            <style global jsx>{`
+  return (
+    <main>
+      <style global jsx>{`
                 * {
                     margin: 0;
                     padding: 0;
@@ -40,24 +40,28 @@ function Home() {
                     scale: 1.025;
                 }
             `}</style>
-            <h1>Compra um petisco para vaquinha</h1>
-            <section>
-                <div>
-                    <img src={foto_vaquinha_2.default.src} alt="Vaquinha"/>
-                </div>
-                <div>
-                    <img src={foto_vaquinha_1.default.src} alt="Vaquinha"/>
-                </div>
-                <div>
-                    <img src={foto_vaquinha_3.default.src} alt="Vaquinha"/>
-                </div>
-            </section>
-            <section>
-                <h2>Ele gosta de:</h2>
-                <img src="https://www.pedigree.com.br/cdn-cgi/image/format=auto,q=90/sites/g/files/fnmzdf2401/files/2024-07/PEDIGREE%20ADULTO%20CARNE%20100g.png" />
-            </section>
-        </main>
-    );
+      <h1>Compra um petisco para vaquinha</h1>
+      <section>
+        <div>
+          <img src={foto_vaquinha_2.default.src} alt="Vaquinha" />
+        </div>
+        <div>
+          <img src={foto_vaquinha_1.default.src} alt="Vaquinha" />
+        </div>
+        <div>
+          <img src={foto_vaquinha_3.default.src} alt="Vaquinha" />
+        </div>
+      </section>
+      <section>
+        <h2>Ele gosta de:</h2>
+        <img src="https://www.pedigree.com.br/cdn-cgi/image/format=auto,q=90/sites/g/files/fnmzdf2401/files/2024-07/PEDIGREE%20ADULTO%20CARNE%20100g.png" />
+      </section>
+    </main>
+  );
+}
+
+function teste() {
+  return "Teste";
 }
 
 export default Home;
